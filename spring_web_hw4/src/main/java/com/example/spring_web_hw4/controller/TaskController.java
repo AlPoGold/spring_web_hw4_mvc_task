5fc7b6fddbfbf8b94f78d5dac25729ecd9f0f0c8
Polina Kolesnikova <polina@example.com>
2024-04-15 15:18:15 +0200

    adding css style

--- spring_web_hw4/src/main/java/com/example/spring_web_hw4/controller/TaskController.java
+++ spring_web_hw4/src/main/java/com/example/spring_web_hw4/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.example.spring_web_hw4.controller;
 import com.example.spring_web_hw4.domain.Task;
 import com.example.spring_web_hw4.service.TaskService;
 import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
+import java.util.logging.Level;
 
 
 @Controller
 @AllArgsConstructor
+@Log
 public class TaskController {
 
     TaskService service;
@@ -34,7 +37,11 @@ public class TaskController {
                                   @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date finishingDate,
                                       Model model){
         service.addTask(description, level, finishingDate);
+        log.log(Level.INFO, "task was added!");
+
+
         model.addAttribute("tasks", service.getTasks());
+
         return "tasks.html";
     }
 
